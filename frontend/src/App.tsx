05fdc0ed75642035e8510ec3cdfbea3474921c02
TheodorSTTH <theodor.learn@gmail.com>
2025-10-22 01:10:12 +0200

    sidebar + tailwind + daisyUI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
+import Sidebar from "./components/sidebar";
 
 export default function App() {
   const [msg, setMsg] = useState("Loading...");
 
   useEffect(() => {
     fetch("/api/ping")
-      .then(r => r.json())
-      .then(data => setMsg(data.message))
-      .catch(() => setMsg("Failed to reach backend"));
+    .then(r => r.json())
+    .then(data => setMsg(data.message))
+    .catch(() => setMsg("Failed to reach backend"));
   }, []);
 
   async function testProtectedData() {
@@ -22,9 +23,10 @@ export default function App() {
       console.error("Error:", error);
     }
   }
-
+  
   async function logout() {
     // read CSRF cookie (if you already have a helper, reuse it)
+    if (!confirm("Are you sure you want to log out?")) return;
     const m = document.cookie.match(/(^|;\s*)csrftoken=([^;]+)/);
     const csrftoken = m ? decodeURIComponent(m[2]) : "";
     await fetch("/accounts/logout/", {
@@ -35,16 +37,30 @@ export default function App() {
     // then reload or route home
     window.location.href = "/";
   }
-  
 
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", padding: 24 }}>
-      <h1>🍣 Drone Delivery Dashboard!</h1>
-      <p>Status from backend: <strong>{msg}</strong></p>
-      <hr />
-      <p>React + TypeScript + Django + Postgres via Docker.</p>
-      <button onClick={testProtectedData}>Test Protected Data</button>
-      <button onClick={logout}>Log out</button>
+    <div className="flex">
+      <Sidebar />
+      <div className="bg-red-200 flex-1">
+        {/* <h1>Hello, World!</h1>
+        <button className="btn">Click me</button> */}
+        <div style={{ fontFamily: "system-ui, sans-serif", padding: 24 }}>
+          <p>Status from backend: <strong>{msg}</strong></p>
+          <button className="btn" onClick={testProtectedData}>Test Protected Data</button>
+          <button className="btn btn-error" onClick={logout}>Log out</button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
+
+
+// export default function App() {
+
+
+
+  
+
+//   return (
+//   );
+// }
