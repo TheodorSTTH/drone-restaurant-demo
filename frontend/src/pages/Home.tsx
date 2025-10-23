4bf05a19af054c1e103f68071384397447ebf129
TheodorSTTH <theodor.learn@gmail.com>
2025-10-23 05:34:06 +0200

    Removing one more unused import

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
-import { csrftoken } from "@/csrf";
 import { Link } from "react-router-dom";
 import { User } from "lucide-react";
 import { menuItems } from "@/components/sidebar";
